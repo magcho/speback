d7b459ac83751f81c26aa42f0147b494fc229b77
magcho <mail@magcho.com>
2020-05-05 01:44:13 +0900

    [wip]

--- app/config/routes.rb
+++ app/config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
+  get 'static_pages/home'
+  get 'static_pages/help'
+  get 'static_pages/guide'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root 'users#index'
-  resources :users
+  root 'static_pages#home'
+  resources :users, only: [:new, :show, :destory]
 end
