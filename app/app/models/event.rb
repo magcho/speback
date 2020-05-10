499e101b21e5be2b165435c9842fef2e03f90402
magcho <mail@magcho.com>
2020-05-10 21:00:13 +0900

    [fix] close validate syntax

--- app/app/models/event.rb
+++ app/app/models/event.rb
@@ -1,8 +1,10 @@
 class Event < ApplicationRecord
+  has_many :Hashtag
+
   validates :name, presence: true
   validates :start_at, presence: true
   validates :close_at, presence: true
-  validates :link, length: { minimum: 255 }
+  validates :link, length: { maximum: 255 }
   validate :checkAfterCloseAt
 
 
@@ -10,8 +12,6 @@ class Event < ApplicationRecord
 
     # 開始時刻より終了時刻の方が後になっているか確認
     def checkAfterCloseAt
-      errors.add(:close_at)
-      self.start_at < self.close_at
+      errors.add(:close_at) unless self.start_at < self.close_at
     end
-    
 end
