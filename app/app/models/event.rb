15fddf81c440ef071b4f0e36d9d6c57d3801b31a
magcho <mail@magcho.com>
2020-05-10 20:16:54 +0900

    [update] event validate

--- app/app/models/event.rb
+++ app/app/models/event.rb
@@ -1,2 +1,17 @@
 class Event < ApplicationRecord
+  validates :name, presence: true
+  validates :start_at, presence: true
+  validates :close_at, presence: true
+  validates :link, length: { minimum: 255 }
+  validate :checkAfterCloseAt
+
+
+  private
+
+    # 開始時刻より終了時刻の方が後になっているか確認
+    def checkAfterCloseAt
+      errors.add(:close_at)
+      self.start_at < self.close_at
+    end
+    
 end
