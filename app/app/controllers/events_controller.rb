7d259648239fff4bbb484aaf048e09840484e2b2
magcho <mail@magcho.com>
2020-05-12 01:50:17 +0900

    [update] event new controller view

--- app/app/controllers/events_controller.rb
+++ app/app/controllers/events_controller.rb
@@ -1,13 +1,19 @@
 class EventsController < ApplicationController
+  include SessionsHelper
 
-def show
-  @event = Event.find(params[:id])
-  @hashtags = @event.Hashtags
-end
+  def show
+    @event = Event.find(params[:id])
+    @hashtags = @event.Hashtags
+  end
+
+  def new
+    unless logged_in?
+      flash[:error] = 'イベントを登録するにはログインしてください。'
+      redirect_to login_path
+    end
+  end
+  
 
 
-def new
-  @event = Event.new
-end
 
 end
