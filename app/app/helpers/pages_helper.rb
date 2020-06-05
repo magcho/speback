78933a22ec29686337b4c159edf8dd04d0334c91
magcho <mail@magcho.com>
2020-06-05 22:28:44 +0900

    [fix] unstart presenter slide error

--- app/app/helpers/pages_helper.rb
+++ app/app/helpers/pages_helper.rb
@@ -22,7 +22,8 @@ module PagesHelper
 
   # プレゼンターによる遠隔操作中にユーザーが操作した時に戻るボタンを表示するかどうか
   def presenter_page_back_button_style_display(mode, slide, page)
-    return 'none' if slide.livecast == 1
+    return 'none' if slide.livecast == false
+    p slide.livecast
     if mode == 'backward' and slide.current_presenter_page_num < page.page_num
       return 'block'
     elsif mode == 'forward' and slide.current_presenter_page_num > page.page_num
@@ -32,6 +33,13 @@ module PagesHelper
     return 'none'
   end
 
-
+  # 発表者のスライドに追従するときはこのリンクを踏む
+  def link_to_current_presenter_page(slide)
+    if slide.livecast == true
+      return slide_page_path(@slide, @slide.current_presenter_page_num)
+    else
+      return ''
+    end
+  end
   
 end
